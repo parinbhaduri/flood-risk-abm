1dadacb64116da8523d75e6ee7548976c4347b62
parinbhaduri <pbb62@cornell.edu>
2022-08-16 11:36:45 -0400

    Added agent and model step functions
    Added agent and model step function to base_model. Finished relocation function to determine agent relocation based on utility

---
+++ src/base_model.jl
@@ -0,0 +1,116 @@
+#Import Packages
+using Agents
+using Random
+
+#Define Agent Type
+
+mutable struct HouseHold <: AbstractAgent
+    id::Int
+    pos::Dims{2}
+    age::Int
+    income::Int
+end
+
+#Initialize model 
+function flood_ABM(;
+    N = 600,
+    M = 30, 
+    griddims = (M, M),
+    c1 = 294707, 
+    c2 = 130553, 
+    c3 = 128990, 
+    c4 = 154887, 
+    risk_averse = 0.5, #Decimal between 0 and 1
+)
+    space = GridSpace(griddims)
+
+    #Calculate Matrices of Housing Properties
+    SqFeet = rand(500:4000, griddims)
+    Age = rand(0:5, griddims)
+    Stories = rand(1:3,griddims)
+    Baths = rand(1:4, griddims)
+    #Future Case: randomly generate values in separate file and save to csv file.
+    #Then load properties from csv file
+
+    properties = Dict(:Elevation => zeros(griddims), :SqFeet => SqFeet,
+     :Age => Age, :Stories => Stories, :Baths => Baths, :risk_averse => risk_averse)
+     #Calculate initial utility
+     init_utility = c1 * SqFeet + c2 * Age + c3 * Stories + c4 * Baths
+     properties[:init_utility] = init_utility
+
+    model = ABM(
+        HouseHold,
+        space;
+        properties = properties,
+    )
+
+    #Add Agents
+    for n in 1:N
+        agent = HouseHold(n, (1,1), rand(1:5), rand(30000:200000))
+        add_agent_single!(agent, model)
+    end
+
+   
+    return model
+end
+
+## Sort agents based on income
+function pop_change!(model::ABM)
+    #Ultimately will add population growth methods
+end
+
+##Update Utility from Flooding
+function update_utility(model::ABM)
+    #Calculate new utility after flood occurs
+
+    #create utility matrix of available cells for agent relocation
+    avail_pos = zeros(30,30)
+    #Iterate through empty gridspaces and add associated utility to avail_pos 
+    for pos in empty_positions(model)
+        avail_pos[pos...] = model.init_utility[pos...] 
+    end
+    return avail_pos
+end 
+
+    
+## Agent Relocation 
+function relocation!(utility_matrix::Matrix, model::ABM)
+    #sort agents by income 
+    sorted_agent = sort(collect(allagents(model)), by = x -> x.income, rev = true)
+
+    #Find max available utility and its position
+    new_max = maximum(utility_matrix)
+    best_ind = findfirst(x -> x == new_max, utility_matrix)
+    #Iterate through agents
+    for i in sorted_agent
+        #If agent's current utility is larger than max available, skip iteration
+        model.init_utility[i.pos...] > new_max && continue
+        #move agent to better utility location
+        move_agent!(i, tuple(best_ind[1], best_ind[2]), model)
+
+        #Remove position and update max terms
+        utility_matrix[best_ind] = 0
+        new_max = maximum(utility_matrix)
+        best_ind = findfirst(x -> x == new_max, utility_matrix)
+    end
+end
+
+
+    ### agent and model step functions
+
+    #Agent_step function used to remove agents over time
+function agent_step!(agent::HouseHold, model::ABM)
+        #track agent age over time
+        agent.age += 1
+        #Function will then remove agent using kill_agent! when age threshold is reached
+
+end
+
+    #model step really defines dynamic progression of simulation
+function model_step!(model::ABM)
+        new_utility = update_utility(model)
+        relocation!(new_utility, model)
+end
+
+
+
