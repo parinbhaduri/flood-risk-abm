da21c59b56ee93f05245186a879d8c55534c3f53
parinbhaduri <pbb62@cornell.edu>
2024-04-19 13:49:10 -0400

    updated ensemble plot attributes
    Changed line colors for risk aversion categories

--- test/ensemble_visual.jl
+++ test/ensemble_visual.jl
@@ -3,7 +3,7 @@ include("../src/visual_attrs.jl")
 
 #For Parallelization
 using Distributed
-addprocs(4, exeflags="--project=$(Base.active_project())")
+addprocs(10, exeflags="--project=$(Base.active_project())")
 
 #initialize model
 @everywhere include("../src/base_model.jl")
@@ -33,15 +33,17 @@ mdf_show = filter(:seed => isequal(1897), mdf)
 ##Plot
 
 #plot agents deciding to move
-agent_plot = Plots.plot(adf.step, adf.count_action_fam, group = adf.risk_averse, label = false, linecolor = [housecolor[7] housecolor[3]], alpha = 0.35, lw = 1)
+agent_plot = Plots.plot(adf.step, adf.count_action_fam, group = adf.risk_averse, label = false, linecolor = [colorant"#0A9396" colorant"#CA6702"], alpha = 0.35, lw = 1, tickfontsize = 10)
 
 Plots.plot!(adf_show.step, adf_show.count_action_fam, group = adf_show.risk_averse, label = ["high RA" "low RA"], 
-legend = :topright, legendfontsize = 12, linecolor = [housecolor[6] housecolor[2]], lw = 3)
+ linecolor = [colorant"#0A9396" colorant"#CA6702"],
+legend = :outerright, legendfontsize = 16, foreground_color_legend=nothing,background_color_legend=nothing, lw = 3)
 Plots.ylims!(0,300)
 Plots.ylabel!("Moving Agents", pointsize = 24)
 
 #plot agents in the floodplain
 fp_plot = Plots.plot(adf.step, adf.count_floodplain_fam, group = adf.risk_averse, label = false, linecolor = [colorant"#0A9396" colorant"#CA6702"], alpha = 0.35, lw = 1, tickfontsize = 10)
+
 Plots.plot!(adf_show.step, adf_show.count_floodplain_fam, group = adf_show.risk_averse, label = ["high RA" "low RA"], 
 legend = :outerright, legendfontsize = 16, foreground_color_legend=nothing,background_color_legend=nothing, linecolor = [colorant"#0A9396" colorant"#CA6702"], lw = 3)
 #Plots.ylabel!("Floodplain Pop.", pointsize = 28)
@@ -59,7 +61,7 @@ Plots.ylims!(0,40)
 #Plots.ylabel!("Flood Depth", pointsize = 28)
 
 #create subplot
-averse_real = Plots.plot(model_plot, fp_plot, layout = (2,1), dpi = 300, size = (800,800))
+averse_real = Plots.plot(model_plot, agent_plot, fp_plot, layout = (3,1), dpi = 300, size = (800,800))
 
 savefig(averse_real, "test/Test_visuals/paramscan_averse_realizations.svg")
 
@@ -88,15 +90,16 @@ mdf_show_levee = filter(:seed => isequal(1897), mdf_levee)
 ##Plot
 
 #plot agents deciding to move
-agent_plot_levee = Plots.plot(adf_levee.step, adf_levee.count_action_fam, group = adf_levee.risk_averse, label = false, linecolor = [housecolor[7] housecolor[3]], alpha = 0.35, lw = 1)
+agent_plot_levee = Plots.plot(adf_levee.step, adf_levee.count_action_fam, group = adf_levee.risk_averse, label = false, linecolor = [colorant"#0A9396" colorant"#CA6702"], alpha = 0.35, lw = 1, tickfontsize = 10)
 
 Plots.plot!(adf_show_levee.step, adf_show_levee.count_action_fam, group = adf_show_levee.risk_averse, label = ["high RA" "low RA"], 
-legendfontsize = 12, linecolor = [housecolor[6] housecolor[2]], lw = 3)
+legend = :outerright, legendfontsize = 16, foreground_color_legend=nothing,background_color_legend=nothing, linecolor = [colorant"#0A9396" colorant"#CA6702"], lw = 3)
 Plots.ylims!(0,300)
 Plots.ylabel!("Moving Agents", pointsize = 24)
 
 #plot agents in the floodplain
 fp_plot_levee = Plots.plot(adf_levee.step, adf_levee.count_floodplain_fam, group = adf_levee.risk_averse, label = false, linecolor = [colorant"#0A9396" colorant"#CA6702"], alpha = 0.35, lw = 1, tickfontsize = 10)
+
 Plots.plot!(adf_show_levee.step, adf_show_levee.count_floodplain_fam, group = adf_show_levee.risk_averse, label = ["high RA" "low RA"], 
 legend = :outerright, legendfontsize = 16, foreground_color_legend=nothing,background_color_legend=nothing, linecolor = [colorant"#0A9396" colorant"#CA6702"], lw = 3)
 #Plots.ylabel!("Floodplain Pop.", pointsize = 28)
@@ -115,6 +118,6 @@ Plots.ylims!(0,40)
 
 
 #create subplot
-levee_real = Plots.plot(model_plot_levee, fp_plot_levee, layout = (2,1), dpi = 300, size = (800,800))
+levee_real = Plots.plot(model_plot_levee, agent_plot_levee, fp_plot_levee, layout = (3,1), dpi = 300, size = (800,800))
 
 savefig(levee_real, "test/Test_visuals/paramscan_levee_realizations.svg")
